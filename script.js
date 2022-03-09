c8522b89eace62e27d43f11885b842f6c7c21154
Dean Lewis <d.lewis.rave@gmail.com>
2022-03-09 21:59:14 +0000

    api not pulling in information - might have exceed call amount for today or browser doesnt support the tools

---
+++ script.js
@@ -0,0 +1,15 @@
+let weather = {
+  "apiKey": "7c8d356661b9b4ca223b3d608f00359a",
+  fetchWeather: function(city) {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" 
+      + city 
+      + "&units=metric&appid=" 
+      + this.apiKey)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  },
+  displayWeather: function(data) {
+
+  }
+};
