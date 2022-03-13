917c25d9ad25e4ef31140f48836499b4a527d591
Dean Lewis <d.lewis.rave@gmail.com>
2022-03-13 14:22:20 +0000

    set default weather to london

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name,icon,description,temp,humidity,speed)
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
      "https://openweathermap.org/img/wn/" + icon +".png";
@@ -39,5 +38,7 @@ document
       weather.search();
     }
   });
+
+weather.fetchWeather("London");
 
 // api working now
