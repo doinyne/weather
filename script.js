6ada56c9d96c9ab6862963313182bb4624f0bb3a
Dean Lewis <d.lewis.rave@gmail.com>
2022-03-13 14:20:14 +0000

    user can hit enter to search for weather report

--- script.js
+++ script.js
@@ -22,7 +22,22 @@ let weather = {
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText = "Humidity: " + humidity +"%";
     document.querySelector(".wind").innerText = "Wind speed: " + speed +"km/h";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
   }
 };
+
+document.querySelector(".search button").addEventListener("click", function() {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
 
 // api working now
